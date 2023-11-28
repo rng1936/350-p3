24a93c28922a3e5b128b1ece069a4a8676f50b80
Ronald Ng <ronaldn1936@gmail.com>
2023-11-27 23:36:51 -0500

    implemented task 3.1

--- sh.c
+++ sh.c
@@ -60,7 +60,7 @@ runcmd(struct cmd *cmd)
   //int p[2];
   //struct backcmd *bcmd;
   struct execcmd *ecmd;
-  //struct listcmd *lcmd;
+  struct listcmd *lcmd;
   //struct pipecmd *pcmd;
   //struct redircmd *rcmd;
   
@@ -84,7 +84,17 @@ runcmd(struct cmd *cmd)
     break;
 
   case LIST:
-    printf(2, "List Not Implemented\n");
+    lcmd = (struct listcmd*)cmd;
+    int pid = fork();
+    if (pid < 0) {
+      printf(1, "list failed\n");
+      break;
+    } else if (pid == 0) {
+      runcmd(lcmd->left);
+    } else {
+      wait();
+      runcmd(lcmd->right);
+    }
     break;
 
   case PIPE:
